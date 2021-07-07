5524205f1cc31c42424d0064d03529dfcfb7a988
santoshmsp <santoshmsp@gmail.com>
2021-07-07 13:23:04 +0530

    Added new Project

---
+++ Utility/MathTech.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public class MathTech
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
